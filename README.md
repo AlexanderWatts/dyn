cc308b7efb0367470cd48b872ae543fae2acba59
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-07-13 17:13:00 +0100

    Added initial dyn class that can run files and repl - does not compute anything but good start

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dyn
 A DYNamically typed general purpose language
+
+## Running dyn
+
+Dyn can be run from file or through REPL/prompt
+
+File:
+
+```bash
+java ./src/Dyn.java examples/<file>.dyn
+```
+
+REPL:
+
+```bash
+java ./src/Dyn.java
+``
---
+++ src/Dyn.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Dyn {
+	public static void main(String[] args) throws IOException {
+		if (args.length > 1) {
+			System.exit(64);
+		} else if (args.length == 1) {
+			runFile(args[0]);
+		} else {
+			repl();
+		}
+	}
+
+	private static void runFile(String path) throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get(path));
+		String source = new String(bytes, Charset.defaultCharset());
+
+		System.out.println("Output:");
+		System.out.println(source);
+	}
+
+	/**
+	 * Run dyn interactively
+	 * REPL (Read Evaluate Print Loop)
+	 *
+	 * Use Ctl-c or Ctl-d to exit
+	 *
+	 * @throws IOException
+	 */
+	private static void repl() throws IOException {
+		System.out.println("Dyn REPL");
+
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader bufferedReader = new BufferedReader(input);
+
+		for (;;) {
+			System.out.print("> ");
+			String line = bufferedReader.readLine();
+			System.out.println(line);
+			
+			if (line == null) {
+				break;
+			}
+		}
+	}
+}
+
