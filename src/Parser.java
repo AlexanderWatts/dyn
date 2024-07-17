a20b683c5d9987e8b8b58a5e38a29a41217ab5ee
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-07-17 12:16:31 +0100

    Created unary method for handling unary productions and added grammar production comment above method for useful reference

--- src/Parser.java
+++ src/Parser.java
@@ -10,9 +10,31 @@ public class Parser {
 
 	public Expr parse() {
 		System.out.println("Parsing...");
+		return unary();
+	}
+
+	/**
+	 * Grammar production
+	 * unary = ("-" | "!") unary | primary ;
+	 */
+	private Expr unary() {
+		if (isCurrentToken(TokenType.MINUS, TokenType.BANG)) {
+			Token operator = getCurrentToken();
+			System.out.println("operator" + operator);
+			getCurrentTokenAndAdvance();
+
+			Expr right = unary();
+			
+			return new Expr.Unary(operator, right);
+		}
+
 		return primary();
 	}
 
+	/**
+	 * Grammar production
+	 * primary = STRING | NUMBER | "true" | "false" | "nil" | "(" expression ")" ;
+	 */
 	private Expr primary() {
 		if (isCurrentToken(TokenType.STRING) || isCurrentToken(TokenType.NUMBER)) {
 			Expr literal = new Expr.Literal(getCurrentToken().getLiteral());
