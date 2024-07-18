f5fc40bbdf727e2674a221305792c90b9168cbea
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-07-18 15:39:44 +0100

    Created helper methods

--- src/Parser.java
+++ src/Parser.java
@@ -12,5 +12,45 @@ public class Parser {
 		System.out.println("Parsing...");
 	}
 
+	private boolean match(TokenType... tokenTypes) {
+
+		for (TokenType tokenType : tokenTypes) {
+			if (check(tokenType)) {
+				getCurrentTokenAndAdvance();
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean check(TokenType tokenType) {
+		if (isAtEnd()) {
+			return false;
+		}
+
+		return getCurrentToken().getType() == tokenType;
+	}
+
+	private Token getCurrentTokenAndAdvance() {
+		if (!isAtEnd()) {
+			current++;
+		}
+
+		return getPreviousToken();
+	}
+
+	private Token getPreviousToken() {
+		return tokens.get(current - 1);
+	}
+
+	private boolean isAtEnd() {
+		return getCurrentToken().getType() == TokenType.EOF;
+	}
+
+
+	private Token getCurrentToken() {
+		return tokens.get(current);
+	}
 }
 
