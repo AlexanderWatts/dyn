38049f24157e7fe6d390871e70c8698a1f2a2d3a
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-07-20 13:49:46 +0100

    Created empty interpreter class utilising the visitor pattern

---
+++ src/Interpreter.java
@@ -0,0 +1,29 @@
+
+public class Interpreter implements Expr.Visitor<Object> {
+
+	@Override
+	public Object visit(Expr.Binary expr) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+	@Override
+	public Object visit(Expr.Grouping expr) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+	@Override
+	public Object visit(Expr.Unary expr) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+	@Override
+	public Object visit(Expr.Literal expr) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+}
+
