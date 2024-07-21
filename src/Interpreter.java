eea14cc9b99830eabeaf01d899246c3bf8cc310a
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-07-21 10:50:14 +0100

    Interpret binary expressions

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -6,8 +6,51 @@ public class Interpreter implements Expr.Visitor<Object> {
 
 	@Override
 	public Object visit(Expr.Binary expr) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+		Object left = evaluate(expr.getLeft());
+		Object right = evaluate(expr.getRight());
+
+		switch (expr.getOperator().getType()) {
+			case TokenType.PLUS: {
+				if (left instanceof String && right instanceof String) {
+					return (String) left + (String) right;
+				}
+
+				if (left instanceof Double && right instanceof Double) {
+					return (double) left + (double) right;
+				}
+				
+				break;
+			}
+			case TokenType.MINUS: {
+				return (double) left - (double) right;
+			}
+			case TokenType.STAR: {
+				return (double) left * (double) right;
+			}
+			case TokenType.SLASH: {
+				return (double) left / (double) right;
+			}
+			case TokenType.GREATER_EQUAL: {
+				return (double) left >= (double) right;
+			}
+			case TokenType.GREATER: {
+				return (double) left > (double) right;
+			}
+			case TokenType.LESS: {
+				return (double) left < (double) right;
+			}
+			case TokenType.LESS_EQUAL: {
+				return (double) left <= (double) right;
+			}
+			case TokenType.EQUAL_EQUAL: {
+				return isEqual(left, right);
+			}
+			case TokenType.BANG_EQUAL: {
+				return !isEqual(left, right);
+			}
+		}
+
+		return null;
 	}
 
 	@Override
@@ -34,6 +77,18 @@ public class Interpreter implements Expr.Visitor<Object> {
 	@Override
 	public Object visit(Expr.Literal expr) {
 		return expr.getValue();
+	}
+
+	private boolean isEqual(Object a, Object b) {
+		if (a == null && b == null) {
+			return true;
+		}
+
+		if (a == null) {
+			return false;
+		}
+
+		return a.equals(b);
 	}
 
 	private boolean isTruthy(Object object) {
